f3b9ceae7d5c73b7a76a72972ea927d8d3f43bdf
Joshua Hutchins <ninjosh97@gmail.com>
2019-10-23 23:48:45 -0400

    Movement, Legend Display, and more!
    In this update we have:
    -More tags (all 6)
    -A legend displaying names of all the tags
    -Tags moving (reading data from file)
    
    The movement math is slightly broken, tags move off the canvas occasionally.
    
    Next steps:
    -Fix movement math
    -Add name entry box

--- End-User-Application/tag.py
+++ End-User-Application/tag.py
@@ -1,21 +1,25 @@
 from tkinter import *
 from PIL import ImageTk, Image
+from time import sleep
+
 class tag:
     tagDiameter = 35
     squareSideLength = 40
     center = (squareSideLength/2)-(tagDiameter/2)
+    thing = 0
 
     def __init__(self, name, x, y, imageName, canvas):
         self.name = name
         self.x = x
         self.y = y
         self.imageObject = ImageTk.PhotoImage(Image.open(imageName))
-        canvas.create_image((self.x*self.squareSideLength)+self.center+2, (self.y*self.squareSideLength)+self.center+2, anchor=NW, image=self.imageObject)
+        self.thing = canvas.create_image(((self.x-1)*self.squareSideLength)+self.center+2, ((self.y-1)*self.squareSideLength)+self.center+2, anchor=NW, image=self.imageObject)
     
     def setCoords(self, x, y):
         self.x = x
         self.y = y
+        print(self.name + " x: " + str(x))
+        print(self.name + " y: " + str(y))
     
     def updatePosition(self, canvas):
-        canvas.coords(self.imageObject, self.x, self.y)
-        canvas.after(5000, updatePosition(canvas))
+        canvas.coords(self.thing, ((self.x-1)*self.squareSideLength)+self.center+2, ((self.y-1)*self.squareSideLength)+self.center+2)
--- End-User-Application/test.py
+++ End-User-Application/test.py
@@ -1,7 +1,16 @@
-import 
 from tag import *
 from tkinter import *
 from PIL import ImageTk, Image
+import threading
+from time import sleep
+import csv
+
+t1name = "Phillip"
+t2name = "Peter Ian Staker"
+t3name = "Chris P Bacon"
+t4name = "Cool Pic Kyle"
+t5name = "Helen Hywater"
+t6name = "Keanu Reeves"
 
 root = Tk()
 root.geometry('1220x600')
@@ -11,8 +20,66 @@ bg = ImageTk.PhotoImage(Image.open("bg.png"))
 c.create_image(2,2, anchor=NW, image=bg)
 c.pack(side = TOP)
 
-tag1 = tag("pbillip", 5, 5, "t1.png")
-tag1.updatePosition(c)
-c.pack()
+def moveAndUpdate():
+    with open("data.csv", 'r', encoding='utf-8-sig') as csvFile: 
+        csvReader = csv.reader(csvFile, delimiter=',')
+        for row in csvReader:
+            tag1.setCoords(int(row[0]), int(row[1]))
+            tag2.setCoords(int(row[2]), int(row[3]))
+            tag3.setCoords(int(row[4]), int(row[5]))
+            tag4.setCoords(int(row[6]), int(row[7]))
+            tag5.setCoords(int(row[8]), int(row[9]))
+            tag6.setCoords(int(row[10]), int(row[11]))
+            
+            tag1.updatePosition(c)
+            tag2.updatePosition(c)
+            tag3.updatePosition(c)
+            tag4.updatePosition(c)
+            tag5.updatePosition(c)
+            tag6.updatePosition(c)
+
+            root.update()
+            sleep(2)
+
+
+tag1 = tag(t1name, 5, 5, "t1.png", c)
+tag2 = tag(t2name, 20, 3, "t2.png", c)
+tag3 = tag(t3name, 10, 1, "t3.png", c)
+tag4 = tag(t4name, 11, 1, "t4.png", c)
+tag5 = tag(t5name, 11, 4, "t5.png", c)
+tag6 = tag(t6name, 12, 3, "t6.png", c)
+
+cLegend = Canvas (root, height = 280, width = 40)
+cLegend.create_image(5,5, anchor = NW, image=tag1.imageObject)
+cLegend.create_image(5,45, anchor = NW, image=tag2.imageObject)
+cLegend.create_image(5,85, anchor = NW, image=tag3.imageObject)
+cLegend.create_image(5,125, anchor = NW, image=tag4.imageObject)
+cLegend.create_image(5,165, anchor = NW, image=tag5.imageObject)
+cLegend.create_image(5,205, anchor = NW, image=tag6.imageObject)
+cLegend.place(x=0, y=330, anchor=NW)
+
+t1Label = Label(root, text=t1name, font=("Helvetica", 18))
+t1Label.place(x = 50, y = 330 + 5)
+
+t2Label = Label(root, text=t2name, font=("Helvetica", 18))
+t2Label.place(x = 50, y = 330 + 45)
+
+t3Label = Label(root, text=t3name, font=("Helvetica", 18))
+t3Label.place(x = 50, y = 330 + 85)
+
+t4Label = Label(root, text=t4name, font=("Helvetica", 18))
+t4Label.place(x = 50, y = 330 + 125)
+
+t5Label = Label(root, text=t5name, font=("Helvetica", 18))
+t5Label.place(x = 50, y = 330 + 165)
+
+t6Label = Label(root, text=t6name, font=("Helvetica", 18))
+t6Label.place(x = 50, y = 330 + 205)
+
+
+t1 = threading.Thread(target=moveAndUpdate, args=()) 
+
+t1.start()
+#c.pack()
 
 root.mainloop()
